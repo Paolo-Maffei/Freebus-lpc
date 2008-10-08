bd908025eadfde8188e85e7a1a880a8760a7a72b
Andreas Krebs <kubi@krebsworld.de>
2008-10-08 20:45:28 +0000

    cleanup
    git-svn-id: file:///usr/local/svn/freebus/trunk@608 219b8a4f-0e80-dc11-b224-0019d16aef49

--- 89LPC922/out8/fb_out8.c
+++ 89LPC922/out8/fb_out8.c
@@ -44,7 +44,57 @@
 #include <P89LPC922.h>
 #include "../com/fb_hal_lpc.h"
 #include "../com/fb_prot.h"
-#include "fb_app_out8.h"
+#include "../com/fb_app_out.h"
+
+
+
+
+void restart_app(void)		// Alle Applikations-Parameter zurücksetzen
+{
+
+  unsigned char bw,bwh,n;
+  
+  P0M1=0x00;				// Port 0 Modus push-pull für Ausgang
+  P0M2=0xFF;
+
+  portbuffer=userram[0x29];	// Verhalten nach Busspannungs-Wiederkehr
+  bw=eeprom[0xF6];
+  for(n=0;n<=3;n++)			// Ausgänge 1-4
+  {
+    bwh=(bw>>(2*n))&0x03; 
+    if(bwh==0x01)  portbuffer=portbuffer & (0xFF-(0x01<<n));
+    if(bwh==0x02)  portbuffer=portbuffer | (0x01<<n);
+  }
+  bw=eeprom[0xF7];
+  for(n=0;n<=3;n++)			// Ausgänge 5-8
+  {
+    bwh=(bw>>(2*n))&0x03; 
+    if(bwh==0x01)  portbuffer=portbuffer & (0xFF-(0x01<<(n+4)));
+    if(bwh==0x02)  portbuffer=portbuffer | (0x01<<(n+4));
+  }
+  port_schalten(portbuffer);  
+  
+  pdir=0xFF;		// Port-Direction - wenn Bit=1 darf entsprechender Portpin beschrieben werden (Ausgang), sonst nicht (Eingang)
+  zfstate=0x00;		// Zustand der Zusatzfunktionen 1-4
+  blocked=0x00;		// Ausgänge nicht gesperrt
+  timer=0;			// Timer-Variable, wird alle 135us inkrementiert
+  
+  logicstate=0;
+  objstate=0;
+  
+  start_writecycle();
+  write_byte(0x01,0x03,0x00);	// Herstellercode 0x0004 = Jung
+  write_byte(0x01,0x04,0x04);
+  write_byte(0x01,0x05,0x20);	// Device Type (2038.10) 2060h
+  write_byte(0x01,0x06,0x60);	// 	"	"	"
+  write_byte(0x01,0x07,0x01);	// Versionsnummer
+  write_byte(0x01,0x0C,0x00);	// PORT A Direction Bit Setting
+  write_byte(0x01,0x0D,0xFF);	// Run-Status (00=stop FF=run)
+  write_byte(0x01,0x12,0x9A);	// COMMSTAB Pointer
+  stop_writecycle();
+}
+
+
 
 
 
@@ -60,13 +110,13 @@ void main(void)
   
   do  {
     if(RTCCON>=0x80) delay_timer();	// Realtime clock Überlauf
-    TASTER=1;				// Pin als Eingang schalten um Taster abzufragen
-    if(!TASTER) {			// Taster gedrückt
+    TASTER=1;						// Pin als Eingang schalten um Taster abzufragen
+    if(!TASTER) {					// Taster gedrückt
       for(n=0;n<100;n++) {}
-      while(!TASTER);			// warten bis Taster losgelassen
+      while(!TASTER);				// warten bis Taster losgelassen
       progmode=!progmode;
     }
-    TASTER=!progmode;			// LED entsprechend schalten (low=LED an)
+    TASTER=!progmode;				// LED entsprechend schalten (low=LED an)
     for(n=0;n<100;n++) {}
   } while(1);
 }
