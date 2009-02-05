db1495d91d995ecc44f0ab68e1f93d0d94423319
Johannes Hub <JohannesHub@t-online.de>
2009-02-05 17:23:28 +0000

    added doc header, no code changes
    git-svn-id: file:///usr/local/svn/freebus/trunk@891 219b8a4f-0e80-dc11-b224-0019d16aef49

--- 89LPC922/com/fb_prot.h
+++ 89LPC922/com/fb_prot.h
@@ -12,6 +12,15 @@
  *  published by the Free Software Foundation.
  *
  */
+/**
+* @file   fb_prot.h
+* @author Andreas Krebs <kubi@krebsworld.de>
+* @date    2008
+* 
+* @brief  Hier sind ausschliesslich die Protokoll-Handling Routinen fuer den 89LPC922
+* 
+* 
+*/
 
 #ifndef FB_PROT
 #define FB_PROT
@@ -21,25 +30,26 @@
 #define ADDRTAB			0x16	// Startadresse der Adresstabelle
 
 extern unsigned char telegramm[23];
-extern unsigned char telpos;		// Zeiger auf nächste Position im Array Telegramm
+extern unsigned char telpos;		// Zeiger auf naechste Position im Array Telegramm
 extern unsigned char cs;			// checksum
 extern bit progmode, connected;		// Programmiermodus, Verbindung steht
 extern unsigned char conh, conl;	// bei bestehender Verbindung phys. Adresse des Kommunikationspartners
-extern unsigned char pcount;		// Paketzähler, Gruppenadresszähler
+extern unsigned char pcount;		// Paketzaehler, Gruppenadresszï¿½hler
 extern bit parity_ok;				// Parity Bit des letzten empfangenen Bytes OK
 extern unsigned char last_tel;
 extern bit transparency;			// wenn 1 dann wird telegramm lokal nicht verarbeitet
 
 
-void timer1(void) interrupt 3;	// Interrupt von Timer 1, 370us keine Busaktivität seit letztem Byte, d.h. Telegramm wurde komplett übertragen
-bit get_ack(void);				// wartet bis Byte empfangen wurde und prüft ob es ein ACK war  <- suboptimal, besser mit timeout !!!
+void timer1(void) interrupt 3;	// Interrupt von Timer 1, 370us keine Busaktivitaet seit letztem Byte,
+										//	 d.h. Telegramm wurde komplett uebertragen
+bit get_ack(void);				// wartet bis Byte empfangen wurde und prueft ob es ein ACK war  <- suboptimal, besser mit timeout !!!
 void send_telegramm(void);		// sendet das Telegramm, das in telegramm[] vorher abgelegt wurde und berechnet die checksum
 void send_ack(void);			// wartet auf Timer 1 wegen korrekter Positionierung und sendet ACK (0xCC)
 void ucd_opr(void);				// UCD Verbindungsaufbau
 void ucd_clr(void);				// UCD Verbindungsabbau
 void get_gat(void);				// group address table aus EEPROM lesen und in array gat[] schreiben
-unsigned char gapos_in_gat(unsigned char gah, unsigned char gal);	// Gruppenadresse in Adresstabelle finden, Rückgabe Positionsnr.
-void ncd_quit(void);			// NCD Quittierung zurück senden. Setzt telegramm Bytes 0 bis 6 !!!
+unsigned char gapos_in_gat(unsigned char gah, unsigned char gal);	// Gruppenadresse in Adresstabelle finden, Rueckgabe Positionsnr.
+void ncd_quit(void);			// NCD Quittierung zurueck senden. Setzt telegramm Bytes 0 bis 6 !!!
 void read_masq(void);			// Maskenversion senden
 void read_memory(void);			// read_memory_request - Speicher auslesen und senden
 void write_memory(void);		// write_memory_request - empfangene Daten in Speicher schreiben
@@ -49,18 +59,18 @@ void read_value_req(void);		// Objektwert lesen angefordert
 
 unsigned char read_objflags(unsigned char objno);	// Objektflags lesen
 
-int find_ga(unsigned char objno);	// Gruppenadresse über Assoziationstabelle finden (erster Eintrag, falls mehrere)
+int find_ga(unsigned char objno);	// Gruppenadresse ueber Assoziationstabelle finden (erster Eintrag, falls mehrere)
 unsigned char find_first_objno(unsigned char gah, unsigned char gal);
 
 
-void restart_prot(void);		// Protokoll-relevante Parameter zurücksetzen
+void restart_prot(void);		// Protokoll-relevante Parameter zuruecksetzen
 
-int read_obj_value(unsigned char objno);	// gibt den Wert eines Objektes zurück
-unsigned char read_obj_type(unsigned char objno);		// gibt den Typ eines Objektes zurück
+int read_obj_value(unsigned char objno);	// gibt den Wert eines Objektes zurueck
+unsigned char read_obj_type(unsigned char objno);		// gibt den Typ eines Objektes zurueck
 bit write_obj_value(unsigned char objno,int objvalue);	// schreibt den aktuellen Wert eines Objektes ins 'RAM'
 
-void write_value_req(void);				// Hauptroutine für Ausgänge schalten gemäß EIS 1 Protokoll (an/aus)
+void write_value_req(void);				// Hauptroutine fuer Ausgaenge schalten gemaess EIS 1 Protokoll (an/aus)
 void read_value_req(void);		// Objektwerte lesen angefordert
-void restart_app(void);			// Alle Applikations-Parameter zurücksetzen
+void restart_app(void);			// Alle Applikations-Parameter zuruecksetzen
 
 #endif
